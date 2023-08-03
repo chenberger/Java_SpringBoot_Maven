85a11bfef394f8b4bb01d8cca0b2fbd48fe82e26
chenberger <chen10.berger@gmail.com>
2023-08-03 22:01:45 +0300

    project skeleton commit

---
+++ CCC_Project/src/main/java/com/ccc/ccc_project/CccProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ccc.ccc_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CccProjectApplication.class, args);
+    }
+
+}
---
+++ CCC_Project/src/main/java/com/ccc/ccc_project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ccc.ccc_project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CccProjectApplication.class);
+    }
+
+}
